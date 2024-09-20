9a7b806fd519a9b5830408a493be24b088902688
Kirill Durnin <kirill2308@mail.ru>
2024-09-20 05:58:07 +0500

    The end

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,19 @@ namespace OOP_Laba4_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int day = int.Parse(textBox1.Text);
+            switch (day) {
+                case 1: textBox2.Text = "0"; break;
+                case 2: textBox2.Text = "2"; break;
+                case 3: textBox2.Text = "2"; break;
+                case 4: textBox2.Text = "6"; break;
+                case 5: textBox2.Text = "6"; break;
+                case 6: textBox2.Text = "0"; break;
+                case 7: textBox2.Text = "0"; break;
+            }
+        }
     }
 }
